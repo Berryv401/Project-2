aeae2d8c12b09c784a3340e80e74d03a10e37128
Bernd van der Lande <berndvanderlande@Bernds-MacBook-Air.local>
2020-04-03 11:48:44 -0400

    Updates

---
+++ precache-manifest.9680b86c9496d0a30151a5cf729b0fb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a96796e116deffca3d543b73589f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afe23f39d592db6df96a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8adaeec31c6f875fe897",
+    "url": "/static/css/main.f0b50ae8.chunk.css"
+  },
+  {
+    "revision": "afe23f39d592db6df96a",
+    "url": "/static/js/2.d59ca052.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d59ca052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8adaeec31c6f875fe897",
+    "url": "/static/js/main.d9c82ff7.chunk.js"
+  },
+  {
+    "revision": "3d3f3abf28ace0d84619",
+    "url": "/static/js/runtime-main.3c48109f.js"
+  }
+]);
